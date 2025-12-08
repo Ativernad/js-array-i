58e493391683a2cea49a913e8f584f6bd80a62ea
Ativernad <110638509+Ativernad@users.noreply.github.com>
2025-12-09 01:35:39 +0200

    Done js-array tasks

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,6 +1,15 @@
-// З масива людей(people) Вам потрібно знайти людину за іменем(name)
+// З масива людей (people) потрібно знайти людину за іменем (name)
 function findByName(people, name) {
-  // Ваш код
+  for (let i = 0; i < people.length; i++) {
+    const person = people[i];
+
+    if (person.name === name) {
+      return person; // повертаємо першого, хто підходить
+    }
+  }
+
+  // якщо нікого не знайшли
+  return undefined;
 }
 
 module.exports = findByName;
